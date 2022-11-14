3c5ae58ff24156cb7cf0f29c41577c8b59f26e32
btr8fe <tom.brandenburg@de.bosch.com>
2022-11-14 13:26:59 +0100

    Added Hello World Gradle

---
+++ src/main/java/tb/py4rpy/Greeter.java
@@ -0,0 +1,7 @@
+package tb.py4rpy;
+
+public class Greeter {
+  public String sayHello() {
+  return "Hello world!";
+  }
+}
---
+++ src/main/java/tb/py4rpy/HelloWorld.java
@@ -0,0 +1,8 @@
+package tb.py4rpy;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+  Greeter greeter = new Greeter();
+  System.out.println(greeter.sayHello());
+  }
+}
